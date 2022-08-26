ed45a287bd52c218eaabd0ed01a9d4bc61d3692b
Robert Brodie <r.brodie@sumoheavy.com>
2022-08-26 09:04:37 -0400

    SP-196 Add return type hints

--- src/BitPaySDK/Model/Invoice/RefundWebhook.php
+++ src/BitPaySDK/Model/Invoice/RefundWebhook.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BitPaySDK\Model\Invoice;
 
 class RefundWebhook
@@ -20,117 +22,117 @@ class RefundWebhook
     {
     }
 
-    public function getId()
+    public function getId(): string
     {
         return $this->_id;
     }
 
-    public function setId(string $id)
+    public function setId(string $id): void
     {
         $this->_id = $id;
     }
 
-    public function getInvoice()
+    public function getInvoice(): string
     {
         return $this->_invoice;
     }
 
-    public function setInvoice(string $invoice)
+    public function setInvoice(string $invoice): void
     {
         $this->_invoice = $invoice;
     }
 
-    public function getSupportRequest()
+    public function getSupportRequest(): string
     {
         return $this->_supportRequest;
     }
 
-    public function setSupportRequest(string $supportRequest)
+    public function setSupportRequest(string $supportRequest): void
     {
         $this->_supportRequest = $supportRequest;
     }
 
-    public function getStatus()
+    public function getStatus(): string
     {
         return $this->_status;
     }
 
-    public function setStatus(string $status)
+    public function setStatus(string $status): void
     {
         $this->_status = $status;
     }
 
-    public function getAmount()
+    public function getAmount(): float
     {
         return $this->_amount;
     }
 
-    public function setAmount(float $amount)
+    public function setAmount(float $amount): void
     {
         $this->_amount = $amount;
     }
 
-    public function getCurrency()
+    public function getCurrency(): string
     {
         return $this->_currency;
     }
 
-    public function setCurrency(string $currency)
+    public function setCurrency(string $currency): void
     {
         $this->_currency = $currency;
     }
 
-    public function getLastRefundNotification()
+    public function getLastRefundNotification(): string
     {
         return $this->_lastRefundNotification;
     }
 
-    public function setLastRefundNotification(string $lastRefundNotification)
+    public function setLastRefundNotification(string $lastRefundNotification): void
     {
         $this->_lastRefundNotification = $lastRefundNotification;
     }
 
-    public function getRefundFee()
+    public function getRefundFee(): float
     {
         return $this->_refundFee;
     }
 
-    public function setRefundFee(float $refundFee)
+    public function setRefundFee(float $refundFee): void
     {
         $this->_refundFee = $refundFee;
     }
 
-    public function getImmediate()
+    public function getImmediate(): bool
     {
         return $this->_immediate;
     }
 
-    public function setImmediate(bool $immediate)
+    public function setImmediate(bool $immediate): void
     {
         $this->_immediate = $immediate;
     }
 
-    public function getBuyerPaysRefundFee()
+    public function getBuyerPaysRefundFee(): bool
     {
         return $this->_buyerPaysRefundFee;
     }
 
-    public function setBuyerPaysRefundFee(bool $buyerPaysRefundFee)
+    public function setBuyerPaysRefundFee(bool $buyerPaysRefundFee): void
     {
         $this->_buyerPaysRefundFee = $buyerPaysRefundFee;
     }
 
-    public function getRequestDate()
+    public function getRequestDate(): string
     {
         return $this->_requestDate;
     }
 
-    public function setRequestDate(string $requestDate)
+    public function setRequestDate(string $requestDate): void
     {
         $this->_requestDate = $requestDate;
     }
 
-    public function toArray()
+    public function toArray(): array
     {
         $elements = [
             'id'                     => $this->getId(),
