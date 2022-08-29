8fd362c8740e70a3184e5de3c6e3df71b77c357d
Robert Brodie <r.brodie@sumoheavy.com>
2022-08-28 20:49:10 -0400

    SP-198 Add entries field to Ledger

--- src/BitPaySDK/Model/Ledger/Ledger.php
+++ src/BitPaySDK/Model/Ledger/Ledger.php
@@ -2,13 +2,36 @@
 
 namespace BitPaySDK\Model\Ledger;
 
+use BitPaySDK\Model\Ledger\LedgerEntry;
+
 class Ledger
 {
+    protected $_entries;
     protected $_currency;
     protected $_balance;
 
     public function __construct()
     {
+    }
+
+    public function getEntries()
+    {
+        $entries = [];
+
+        foreach ($this->_entries as $entry) {
+            if ($entry instanceof LedgerEntry) {
+                array_push($entries, $entry->toArray());
+            } else {
+                array_push($entries, $entry);
+            }
+        }
+
+        return $entries;
+    }
+
+    public function setEntries(array $entries)
+    {
+        $this->_entries = $entries;
     }
 
     public function getCurrency()
