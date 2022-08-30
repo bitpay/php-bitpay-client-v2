76e1fcf2ebad9c6293a5e94ef9111e51871b715d
Robert Brodie <r.brodie@sumoheavy.com>
2022-08-29 20:22:47 -0400

    SP-198 Update code style

--- src/BitPaySDK/Model/Invoice/TransactionDetails.php
+++ src/BitPaySDK/Model/Invoice/TransactionDetails.php
@@ -4,9 +4,9 @@ namespace BitPaySDK\Model\Invoice;
 
 class TransactionDetails
 {
-  protected $_amount;
-  protected $_description;
-  protected $_isFee;
+    protected $_amount;
+    protected $_description;
+    protected $_isFee;
 
   /**
    * Constructs a TransactionDetails object.
@@ -15,51 +15,51 @@ class TransactionDetails
    * @param string $description The three digit currency string.
    * @param bool   $isFee       Designates if the amount is a fee.
    */
-  public function __construct(float $amount, string $description, bool $isFee)
-  {
-    $this->_amount = $amount;
-    $this->_description = $description;
-    $this->_isFee = $isFee;
-  }
+    public function __construct(float $amount, string $description, bool $isFee)
+    {
+        $this->_amount = $amount;
+        $this->_description = $description;
+        $this->_isFee = $isFee;
+    }
 
-  public function getAmount(): float
-  {
-    return $this->_amount;
-  }
+    public function getAmount(): float
+    {
+        return $this->_amount;
+    }
 
-  public function setAmount(float $amount)
-  {
-    $this->_amount = $amount;
-  }
+    public function setAmount(float $amount)
+    {
+        $this->_amount = $amount;
+    }
 
-  public function getDescription(): string
-  {
-    return $this->_description;
-  }
+    public function getDescription(): string
+    {
+        return $this->_description;
+    }
 
-  public function setDescription(string $description)
-  {
-    $this->_description = $description;
-  }
+    public function setDescription(string $description)
+    {
+        $this->_description = $description;
+    }
 
-  public function getIsFee(): bool
-  {
-    return $this->_isFee;
-  }
+    public function getIsFee(): bool
+    {
+        return $this->_isFee;
+    }
 
-  public function setIsFee(bool $isFee)
-  {
-    $this->_isFee = $isFee;
-  }
+    public function setIsFee(bool $isFee)
+    {
+        $this->_isFee = $isFee;
+    }
 
-  public function toArray()
-  {
-    $elements = [
-      'amount'        => $this->getAmount(),
-      'description'   => $this->getDescription(),
-      'isFee'         => $this->getIsFee()
-    ];
+    public function toArray()
+    {
+        $elements = [
+        'amount'        => $this->getAmount(),
+        'description'   => $this->getDescription(),
+        'isFee'         => $this->getIsFee()
+        ];
 
-    return $elements;
-  }
+        return $elements;
+    }
 }
