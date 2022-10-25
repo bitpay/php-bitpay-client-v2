953a8983edfafead0ef750bd39a283bfe80f8d8d
therevix <EH88qbdm3dj9xdULjAXm>
2022-10-25 13:29:11 +0200

    SP-186 - PHP (Full) - PHPDoc - Model/Subscription/Item.php

--- src/BitPaySDK/Model/Subscription/Item.php
+++ src/BitPaySDK/Model/Subscription/Item.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @author BitPay Integrations <integrations@bitpay.com>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+
 namespace BitPaySDK\Model\Subscription;
 
 class Item
@@ -15,36 +20,70 @@ class Item
         $this->_description = $description;
     }
 
+    /**
+     * Gets Line item description
+     *
+     * @return string the Line item description
+     */
     public function getDescription()
     {
         return $this->_description;
     }
 
+    /**
+     * Sets Line item description
+     *
+     * @param string $description the Line item description
+     */
     public function setDescription(string $description)
     {
         $this->_description = $description;
     }
 
+    /**
+     * Gets Line item unit price for the corresponding currency
+     *
+     * @return float the price
+     */
     public function getPrice()
     {
         return $this->_price;
     }
 
+    /**
+     * Sets Line item unit price for the corresponding currency
+     *
+     * @param float $price the price
+     */
     public function setPrice(float $price)
     {
         $this->_price = $price;
     }
 
+    /**
+     * Gets Line item number of units
+     *
+     * @return int the quantity
+     */
     public function getQuantity()
     {
         return $this->_quantity;
     }
 
+    /**
+     * Sets Line item number of units
+     *
+     * @param int $quantity the quantity
+     */
     public function setQuantity(int $quantity)
     {
         $this->_quantity = $quantity;
     }
 
+    /**
+     * @param array $item List of line items
+     * @return Item
+     */
     public static function createFromArray(array $item)
     {
         $instance = new self();
@@ -56,6 +95,11 @@ class Item
         return $instance;
     }
 
+    /**
+     * Gets Item as array
+     *
+     * @return array Item as array
+     */
     public function toArray()
     {
         $elements = [
