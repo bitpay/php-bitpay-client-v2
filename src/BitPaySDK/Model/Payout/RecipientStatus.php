c70e4b532cbd951630b9f21446120a86d603daa0
Marcin Warzybok <m.warzybok@sumoheavy.com>
2022-10-28 14:23:29 +0200

    SP-176 PHP (Full) - PHPDoc - Model/Payout/RecipientStatus.php

--- src/BitPaySDK/Model/Payout/RecipientStatus.php
+++ src/BitPaySDK/Model/Payout/RecipientStatus.php
@@ -1,13 +1,46 @@
 <?php
 
+/**
+ * @author BitPay Integrations <integrations@bitpay.com>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+
 namespace BitPaySDK\Model\Payout;
 
+/**
+ * Interface RecipientStatus
+ * @package BitPaySDK\Model\Payout
+ */
 interface RecipientStatus
 {
+    /**
+     * Invited recipient status.
+     */
     const INVITED    = 'invited';
+
+    /**
+     * Unverified  recipient status.
+     */
     const UNVERIFIED = 'unverified';
+
+    /**
+     * Verified recipient status.
+     */
+
     const VERIFIED   = 'verified';
+
+    /**
+     * Active recipient status.
+     */
     const ACTIVE     = 'active';
+
+    /**
+     * Paused recipient status.
+     */
     const PAUSED     = 'paused';
+
+    /**
+     * Removed recipient status.
+     */
     const REMOVED    = 'removed';
 }
