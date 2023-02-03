5c8849a2a9342489df77643497411d2b69ea1dba
Wojciech Kogut <w.kogut@sumoheavy.com>
2023-02-03 12:24:51 +0100

    SP-417 PHP 8.0.0 Functional test suite

--- test/Integration/BitPaySDK/PayoutClientTest.php
+++ test/Integration/BitPaySDK/PayoutClientTest.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 namespace BitPaySDK\Integration;
 
 use BitPaySDK\Client;
-use BitPaySDK\Exceptions\PayoutCreationException;
-use BitPaySDK\Exceptions\PayoutQueryException;
+use BitPaySDK\Exceptions\BitPayException;
 use BitPaySDK\Model\Payout\Payout;
 use BitPaySDK\Model\Currency;
+use BitPaySDK\Model\Payout\PayoutRecipient;
+use BitPaySDK\Model\Payout\PayoutRecipients;
 use PHPUnit\Framework\TestCase;
 
 class PayoutClientTest extends TestCase
@@ -16,33 +17,66 @@ class PayoutClientTest extends TestCase
 
     public function setUp(): void
     {
-        $this->client = Client::createWithFile('src/BitPaySDK/config.yml');
+        $this->client = Client::createWithFile(Config::INTEGRATION_TEST_PATH . DIRECTORY_SEPARATOR . Config::BITPAY_CONFIG_FILE);
     }
 
-    public function testSubmitPayoutShouldCatchRestCliException(): void
+    public function testPayoutRequests()
     {
         $currency = Currency::USD;
         $ledgerCurrency = Currency::USD;
-        $payout = new Payout(10, $currency, $ledgerCurrency);
-        $payout->setEmail('test1234@example.com');
+        $amount = 10;
+        $email = $this->getFromFile(Config::INTEGRATION_TEST_PATH . DIRECTORY_SEPARATOR . 'email.txt');
+        $submitPayout = $this->submitPayout($currency, $ledgerCurrency, $amount);
+        $this->assertEquals($currency, $submitPayout->getCurrency());
+        $payoutId = $submitPayout->getId();
+        $payout = $this->client->getPayout($payoutId);
+        $this->assertEquals(10, $payout->getAmount());
+        $this->assertEquals($email, $payout->getNotificationEmail());
 
-        $this->expectException(PayoutCreationException::class);
-        $this->client->submitPayout($payout);
-    }
-
-    public function testGetPayoutShouldCatchRestCliException(): void
-    {
-        $this->expectException(PayoutQueryException::class);
-        $this->client->getPayout('test');
-    }
-
-    public function testGetPayouts(): void
-    {
         $startDate = '2022-10-20T13:00:45.063Z';
         $endDate = '2023-01-01T13:00:45.063Z';
 
         $payouts = $this->client->getPayouts($startDate, $endDate);
         $this->assertTrue(is_array($payouts));
         $this->assertCount(count($payouts), $payouts);
+
+        $requestPayoutNotification = $this->client->requestPayoutNotification($payoutId);
+        $this->assertTrue($requestPayoutNotification);
+
+        $cancelledPayout = $this->client->cancelPayout($payoutId);
+        $this->assertTrue($cancelledPayout);
+    }
+
+    private function submitPayout(string $currency, string $ledgerCurrency, int $amount)
+    {
+        $email = $this->getFromFile(Config::INTEGRATION_TEST_PATH . DIRECTORY_SEPARATOR . 'email.txt');
+        $payout = new Payout($amount, $currency, $ledgerCurrency);
+
+        $recipientsList = [
+            new PayoutRecipient(
+                $email,
+                "recipient1",
+                "https://yournotiticationURL.com/b3sarz5bg0wx01eq1bv9785amx")
+        ];
+
+        $recipients = new PayoutRecipients($recipientsList);
+        $payoutRecipients = $this->client->submitPayoutRecipients($recipients);
+        $payoutRecipientId = $payoutRecipients[0]->getId();
+
+        $payout->setRecipientId($payoutRecipientId);
+        $payout->setNotificationURL("https://somenotiticationURL.com");
+        $payout->setNotificationEmail($email);
+        $payout->setReference("PHP Integration tests " . uniqid());
+
+        return $this->client->submitPayout($payout);
+    }
+
+    private function getFromFile(string $path): string
+    {
+        if (!file_exists($path)) {
+            throw new BitPayException("File not found");
+        }
+
+        return file_get_contents($path);
     }
 }
