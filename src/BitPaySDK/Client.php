2d3db28192bca8104c204150ef9d3c54b077195d
Robert Brodie <r.brodie@sumoheavy.com>
2023-06-19 13:58:29 -0400

    SP-604 Update documentation

--- src/BitPaySDK/Client.php
+++ src/BitPaySDK/Client.php
@@ -59,8 +59,9 @@ use Symfony\Component\Yaml\Yaml;
 /**
  * Class Client
  * @package Bitpay
- * @author  Antonio Buedo
- * @version 7.1.0
+ * @author BitPay Integrations <integrations@bitpay.com>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ * @version 8.0.0
  * See bitpay.com/api for more information.
  * date 15.11.2021
  */
