d059d81e12223b561316aad77ae861374673fb54
Marcin Warzybok <m.warzybok@sumoheavy.com>
2023-07-05 12:09:28 +0200

    SP-613 PHP - Bump Version

--- src/BitPaySDK/Env.php
+++ src/BitPaySDK/Env.php
@@ -13,7 +13,7 @@ interface Env
     public const TEST_URL = "https://test.bitpay.com/";
     public const PROD_URL = "https://bitpay.com/";
     public const BITPAY_API_VERSION = "2.0.0";
-    public const BITPAY_PLUGIN_INFO = "BitPay_PHP_Client_v8.0.0";
+    public const BITPAY_PLUGIN_INFO = "BitPay_PHP_Client_v8.0.1";
     public const BITPAY_API_FRAME = "std";
     public const BITPAY_API_FRAME_VERSION = "1.0.0";
 }
